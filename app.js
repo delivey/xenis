fe3af8fb19afd9033ccab593141863c8fa408c01
delivey <deliveydev@gmail.com>
2020-08-21 18:14:01 +0300

    added mongodb connection'

--- app.js
+++ app.js
@@ -2,6 +2,21 @@ const express = require('express') // Import for Express.js
 const app = express();
 const port = 5000; // The port the app will run on
 
+var mongoose = require("mongoose");
+
+require('dotenv').config();
+
+// Mongose and MongoDB setup
+
+var URI = process.env.MONGO_URI;
+mongoose.connect(URI, { useNewUrlParser: true, useUnifiedTopology: true }); 
+
+const Schema = mongoose.Schema;
+
+var urlSchema = new Schema({
+    original_url: { type: String, required: true },
+    new_url : { type: String, required: true }
+});
 
 // GET routes
 
